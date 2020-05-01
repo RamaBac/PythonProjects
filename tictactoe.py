c337fafc7fd038644d56a2e876e3461e9471fbd5
RamaBac <60048491+RamaBac@users.noreply.github.com>
2020-05-01 09:25:18 -0500

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -5,8 +5,9 @@ print("    |    |    ")
 poslst = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 y = 0
 for i in range(9) :
+    if y == 1 : break
     o = input("Enter your position: ")
-    if poslst[int(o)-1] == "X" or poslst[int(o)-1] == "O" or int(o) < 1 or int(o) >= 9 or int(o)%1 != 0 or o == "" :
+    if poslst[int(o)-1] == "X" or poslst[int(o)-1] == "O" or int(o) < 1 or int(o) > 9 or int(o)%1 != 0 or o == "" :
         print("You tried to cheat. You lose your turn.")
         continue
         # i starts from 0 -8
@@ -19,25 +20,26 @@ for i in range(9) :
     print(" ", poslst[3], "  | ", poslst[4], "  | ", poslst[5], "   ")
     print("___________________")
     print(" ", poslst[6], "  | ", poslst[7], "  | ", poslst[8], "   ")
-	a = ['X', 'X', 'X']
-	b = ['O', 'O', 'O']
-    for i in range (0, 3, 6):
-		if poslst[i] == poslst[i+1] == poslst[i+2] :
-			print(poslst[i], "is the winner")
-			y = y+1
-			break
-	for i in range (0, 1, 2) :
-		if poslst[i] == poslst[i+3] == poslst[i+6] :
-			print(poslst[i], "is the winner")
-			y = y + 1
-			break
-	if poslst[0] == poslst[4] == poslst[8] :
-		print(poslst[0], "is the winner")
-		y = y + 1
-		break
-	elif poslst[2] == poslst[4] == poslst[6] :
-		print(poslst[2], "is the winner")
-		y = y + 1
-		break
-if y != 1 :
-    print("It's a draw")  
+    a = ['X', 'X', 'X']
+    b = ['O', 'O', 'O']
+
+    for i in [0, 3, 6]:
+       if [poslst[i], poslst[i+1], poslst[i+2]] == a or [poslst[i], poslst[i+1], poslst[i+2]] == b :
+           print(poslst[i], "is the winner!")
+           y == y + 1
+           break
+    for i in [0, 1, 2] :
+        if [poslst[i], poslst[i+3], poslst[i+6]] == a or [poslst[i], poslst[i+3], poslst[i+6]] == b :
+            print(poslst[i], "is the winner!")
+            y = y + 1
+            break
+    if poslst[0] == poslst[4] == poslst[8] == 'X' or poslst[0] == poslst[4] == poslst[8] == 'O' :
+        print(poslst[0], "wins!")
+        y = y + 1
+        break
+    elif poslst[2] == poslst[4] == poslst[6] == 'X' or poslst[2] == poslst[4] == poslst[6] == 'O' :
+        print(poslst[2], "wins!")
+        y = y + 1
+        break
+if y == 0 :
+    print("It's a draw!")
